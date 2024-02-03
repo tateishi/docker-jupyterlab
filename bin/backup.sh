fdc7b70794c258659e26d89ed0b205be54e4c228
Tateishi Tadatoshi <ishio39@gmail.com>
2024-02-03 11:55:22 +0900

    不要なファイルを削除

--- bin/backup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-
-CONTAINER=docker-jupyterlab_lab_1
-NOW=`date '+%Y%m%d-%H%M%S'`
-NAME=${1:-jupyter}
-BACKUP=/mnt/nfs/backup
-
-docker exec ${CONTAINER} tar czCf work - . > ${BACKUP}/${NAME}-${NOW}.tar.gz
--- bin/restore.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-
-CONTAINER=docker-jupyterlab_lab_1
-
-cat $1 | docker exec -i ${CONTAINER} tar xvzCf work -
